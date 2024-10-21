fc27285cdda0874c6e28f602aab57e2f8ad7f284
mukeshbhattmb <mukeshbhattmab@gmail.com>
2024-10-21 11:42:17 +0530

    added users endpoints

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,4 +1,4 @@
-import { type } from "express/lib/response"
+// import { type } from "express/lib/response"
 import mongoose, { Schema } from "mongoose"
 
 const subscriptionSchema = new Schema(
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,12 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controllers.js";
+import { 
+    registerUser, 
+    loginUser, 
+    logoutUser, 
+    refreshAccessToken, 
+    changeCurrentPassword,
+    getCurrentUser
+} from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -23,8 +30,10 @@ router.route("/login").post(loginUser)
 
 // Secured routes
 
-router.route("/logout").post(verifyJWT,logoutUser)
+router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/get-current-user").get(verifyJWT, getCurrentUser)
 
 
 export default router;
